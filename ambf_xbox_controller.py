65b0768a36912e0dbbd37bf759f55d57d820dbf3
nataliechalfant <natchalfant@gmail.com>
2023-06-20 15:22:04 -0400

    Add experimental rumble support

--- ambf_xbox_controller.py
+++ ambf_xbox_controller.py
@@ -1,14 +1,20 @@
+import inputs
 from inputs import get_gamepad
+from inputs import devices
 import math
 import threading
 
 
 class XboxController(object):
+    """
+    Gets the state of xbox controller inputs using the inputs library. Modified from
+    https://stackoverflow.com/questions/46506850/how-can-i-get-input-from-an-xbox-one-controller-in-python
+    """
     MAX_TRIG_VAL = math.pow(2, 8)
     MAX_JOY_VAL = math.pow(2, 15)
 
     def __init__(self):
-
+        self.gamepad = devices.gamepads[0]
         self.LeftJoystickY = 0
         self.LeftJoystickX = 0
         self.RightJoystickY = 0
@@ -44,11 +50,22 @@ class XboxController(object):
         ry = self.RightJoystickY
         rt = self.RightTrigger
         rb = self.RightBumper
-        return [[lx, ly, lt, lb], [rx, ry, rt, rb]]
+        return [[lx, ly, lt, lb], [rx, ry, rt, rb],
+                [self.A, self.B, self.X, self.Y, self.Back, self.Start]]
+
+    def rumble(self, left, right, time):
+        try:
+            self.gamepad.set_vibration(left, right, time)
+        except OSError:
+            print("no space left on device")
 
     def _monitor_controller(self):
         while True:
-            events = get_gamepad()
+            try:
+                events = get_gamepad()
+            except inputs.UnknownEventCode:
+                print("unknown event")
+
             for event in events:
                 if event.code == 'ABS_Y':
                     self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
@@ -92,7 +109,9 @@ class XboxController(object):
                     self.DownDPad = event.state
 
 
-# if __name__ == '__main__':
-#     joy = XboxController()
-#     while True:
-#         print(joy.read())
+if __name__ == '__main__':
+    joy = XboxController()
+    joy.rumble(1, 1, 100)
+
+    while True:
+        print(joy.read())
