f8883c9042586b5814cc000bbb3e7fe5c398a87e
nataliechalfant <natchalfant@gmail.com>
2023-07-20 21:22:01 -0400

    Create functions to iterate home, sine dance, and move

--- ambf_raven.py
+++ ambf_raven.py
@@ -9,7 +9,7 @@ import ambf_raven_def as ard
 import timeit
 
 '''
-author: Sean Fabrega
+author: Natalie Chalfant, Sean Fabrega
 ambf_raven defines methods for an ambf_raven robot, including homnig, sine dance and hoping soon
 cube tracing and soft body manipulation'''
 
@@ -45,14 +45,31 @@ class ambf_raven:
         self.finished = False
         self.man_steps = 30
 
-    def go_home(self, first_entry, arm, count):
-        '''
+    def go_home(self):
+        """
+        uses the helper function go_home_increment to move raven to its home position
+        """
+        for i in range(self.loop_rate):
+            if not i:
+                print("starting homing")
+                # moves raven incrementally towards home position, if home position is reached, returns True
+                self.homed[0] = self.go_home_increment(1, 1, i)
+                self.homed[1] = self.go_home_increment(1, 0, i)
+            else:
+                self.homed[0] = self.go_home_increment(0, 1, i)
+                self.homed[1] = self.go_home_increment(0, 0, i)
+            time.sleep(0.01)
+        if self.homed[0] and self.homed[1]:
+            print("Raven is homed!")
+
+    def go_home_increment(self, first_entry, arm, count):
+        """
         first entry --> bool
         arm --> bool (0 for left, 1 for right)
         count --> int
-        '''
+        """
         # if first time calling go home
-        if (first_entry):
+        if first_entry:
             for i in range(self.arms[arm].get_num_joints()):
                 self.start_jp[arm][i] = self.arms[arm].get_joint_pos(i)
                 self.delta_jp[arm][i] = self.home_joints[i] - self.arms[arm].get_joint_pos(i)
@@ -69,14 +86,27 @@ class ambf_raven:
 
         max_value = np.max(diff_jp)
 
-        if (max_value < 0.1):
+        if max_value < 0.1:
             self.homed[arm] = True
 
         else:
             self.homed[arm] = False
         return self.homed[arm]
 
-    def sine_dance(self, first_entry, arm, count, rampup_count):
+    def sine_dance(self):
+        if self.i == 0:
+            # start = time.time()
+            # similar to homing, moves raven incrementally in a sine pattern
+            self.sine_dance_increment(1, 1, self.i, self.rampup_count)
+            self.sine_dance_increment(1, 0, self.i, self.rampup_count)
+        else:
+            self.sine_dance_increment(0, 1, self.i, self.rampup_count)
+            self.sine_dance_increment(0, 0, self.i, self.rampup_count)
+
+        self.i += 1
+        time.sleep(0.01)
+
+    def sine_dance_increment(self, first_entry, arm, count, rampup_count):
         self.homed[arm] = False
         for i in range(self.raven_joints):
             offset = (i + arm) * math.pi / 2
@@ -87,7 +117,7 @@ class ambf_raven:
             self.rampup_count[arm] += 1
 
     def get_t_command(self):
-        return (self.arms[0].get_torque_command(), self.arms[1].get_torque_command)
+        return self.arms[0].get_torque_command(), self.arms[1].get_torque_command
 
     def get_raven_status(self, time_now, first_entry=False):
         if first_entry:
@@ -124,10 +154,10 @@ class ambf_raven:
             return status
 
     def set_raven_pos(self, pos_list):
-        '''
+        """
         sets raven position based on array containing positions from the physical
         raven robot. offsets are approximate and need finalization. indexing is intuitive
-        '''
+        """
         for i in range(len(pos_list)):
             if i == 0:
                 self.arms[0].set_joint_pos(i, np.deg2rad(pos_list[i]) + (math.pi / 6))
@@ -159,9 +189,9 @@ class ambf_raven:
                 self.arms[1].set_joint_pos(i - 9, np.deg2rad(pos_list[i]) - math.pi / 12)
 
     def set_raven_force(self, pos_list):
-        '''
+        """
         a prototype for a similar method except using force instead of joint position
-        '''
+        """
         scale = 1
         for i in range(len(pos_list)):
             if i == 0:
@@ -211,6 +241,7 @@ class ambf_raven:
             curr_tm = fk.fwd_kinematics_p5(arm, curr_jp)
         else:
             curr_tm = fk.fwd_kinematics(arm, curr_jp)
+        # print("initial tm :", curr_tm)
         curr_tm[0, 3] += x
         curr_tm[1, 3] += y
         curr_tm[2, 3] += z
@@ -219,23 +250,35 @@ class ambf_raven:
         else:
             jpl = ik.inv_kinematics(arm, curr_tm, gangle)
         self.limited[arm] = jpl[1]
+        # print("new jp: ", jpl)
         if self.limited[arm]:
             print("Desired cartesian position is out of bounds for Raven2. Will move to max pos.")
         new_jp = jpl[0]
         self.next_jp[arm] = new_jp
-        for i in range(len(new_jp)):
-            print("diff: ", new_jp[i]-curr_jp[i])
+        # for i in range(len(new_jp)):
+            # print("diff: ", new_jp[i]-curr_jp[i])
 
         # print("fk ", timeit.timeit(lambda: fk.fwd_kinematics_p5(arm, curr_jp), setup="pass",number=1))
         # print("ik ", timeit.timeit(lambda: ik.inv_kinematics_p5(arm, curr_tm, gangle), setup="pass", number=1))
 
-    def move(self, first_entry, arm, count):
-        '''
-        uses the helper function manual move to slowly increment the robot's position until
-        it reaches the desired inputted position. uses a similar method structure to home
-        '''
+    def plan_move_abs(self, arm, tm, gangle, p5=False, home_dh=ard.HOME_DH):
+        if p5:
+            jpl = ik.inv_kinematics_p5(arm, tm, gangle, home_dh)
+        else:
+            jpl = ik.inv_kinematics(arm, tm, gangle)
+        self.limited[arm] = jpl[1]
+        if self.limited[arm]:
+            print("Desired cartesian position is out of bounds for Raven2. Will move to max pos.")
+        new_jp = jpl[0]
+        self.next_jp[arm] = new_jp
 
-        if (first_entry):
+    def move_increment(self, first_entry, arm, count):
+        """
+        slowly increment the robot's position until it reaches the desired
+        inputted position. uses a similar method structure to home
+        """
+
+        if first_entry:
             for i in range(self.raven_joints):
                 self.start_jp[arm][i] = self.arms[arm].get_joint_pos(i)
                 self.delta_jp[arm][i] = self.next_jp[arm][i] - self.arms[arm].get_joint_pos(i)
@@ -252,12 +295,26 @@ class ambf_raven:
 
         max_value = np.max(diff_jp)
 
-        if (max_value < 0.1 or self.limited[arm]):
+        if max_value < 0.01 or self.limited[arm]:
             self.moved[arm] = True
 
         else:
             self.moved[arm] = False
         return self.moved[arm]
+
+    def move(self):
+        """
+        Uses the helper function move increment to move to the next planned position
+        over a number of steps equal to man_steps
+        """
+        for i in range(self.man_steps):
+            if not i:
+                self.moved[0] = self.move_increment(1, 1, i)
+                self.moved[1] = self.move_increment(1, 0, i)
+            else:
+                self.moved[0] = self.move_increment(0, 1, i)
+                self.moved[1] = self.move_increment(0, 0, i)
+            time.sleep(0.005)
 
     def move_now(self, arm):
         """
@@ -274,7 +331,7 @@ class ambf_raven:
 
         max_value = np.max(diff_jp)
 
-        if (max_value < 0.1 or self.limited[arm]):
+        if max_value < 0.1 or self.limited[arm]:
             self.moved[arm] = True
 
         else:
