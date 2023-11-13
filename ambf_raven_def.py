fix home position for ambf raven, move rotation to be applied for base frame transformation

ambf_raven_def.py
@@ -25,7 +25,8 @@ Eps          = sys.float_info.epsilon
 
 MAX_JOINTS         = np.array([  m.pi,         m.pi,  0.10,            m.pi,   2, (m.pi * 3)/4, (m.pi * 3)/4],  dtype = 'float')
 MIN_JOINTS         = np.array([ -m.pi,        -m.pi, -0.17,           -m.pi,  -2,            0,            0],  dtype = 'float')
-HOME_JOINTS        = np.array([m.pi/3, (m.pi * 3)/5, -0.03,    (m.pi * 3)/4,   0,       m.pi/6,       m.pi/6],  dtype = 'float')
+# HOME_JOINTS        = np.array([m.pi / 6 + , m.pi / 2, 0.03090909, 0, 0, m.pi / 4, m.pi / 4],dtype = 'float')
+HOME_JOINTS        = np.array([m.pi/6 + 5 *Deg2Rad, m.pi/2, -0.07,    (m.pi * 3)/4,   0,       m.pi/6,       m.pi/6],  dtype = 'float')
 HOME_DH            = np.array([[1.04719755, 1.88495559, -0.03, 2.35619449 - m.pi / 2, 0., 0., 0.52359878],
                                 [1.04719755, 1.88495559, -0.03, 2.35619449 - m.pi / 2, 0., -0., 0.52359878]],
                                 dtype="float")
@@ -57,11 +58,16 @@ RAVEN_T_B0         = np.array([np.matrix([[0, 0,  1,  0.30071], [0, -1, 0, 0.061
                                np.matrix([[0, 0, -1, -0.30071], [0,  1, 0, 0.061], [1, 0, 0, -0.007], [0, 0, 0, 1]], dtype = 'float')])
 RAVEN_T_CB         = np.matrix([[0.0, 1.0, 0.0, 0.0],[-1.0, 0.0, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]) #using quaternion to rotational translator
 
+Z_ROT = np.array([np.matrix([[m.cos(np.deg2rad(25)), -m.sin(np.deg2rad(25)), 0, 0], [m.sin(np.deg2rad(25)), m.cos(np.deg2rad(25)), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype='float'),
+                  np.matrix([[m.cos(np.deg2rad(-25)), -m.sin(np.deg2rad(-25)), 0, 0], [m.sin(np.deg2rad(-25)), m.cos(np.deg2rad(-25)), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype='float')])
+
 x_rot_val = 25
 X_ROT = np.matrix([[1, 0, 0, 0],
                     [0, m.cos(np.deg2rad(x_rot_val)), m.sin(np.deg2rad(x_rot_val)), 0],
                     [0, -m.sin(np.deg2rad(x_rot_val)), m.cos(np.deg2rad(x_rot_val)), 0],
                     [0, 0, 0, 1]], dtype='float')
+
+IDENTITY = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype='float')
 
 # Raven joints:
 # joint -: 0_link-base_link_L:             fixed

physical_raven.py
@@ -19,7 +19,10 @@ cube tracing and soft body manipulation'''
 class physical_raven:
     def __init__(self):
 
-        rospy.init_node('raven_keyboard_controller', anonymous=True)
+        try:
+            rospy.init_node('raven_keyboard_controller', anonymous=True)
+        except:
+            pass
 
         self.arm_ctl_l = physical_raven_arm(name_space = ' ', robot_name = 'arm1', grasper_name = 'grasp1')
         self.arm_ctl_r = physical_raven_arm(name_space = ' ', robot_name = 'arm2', grasper_name = 'grasp2')
@@ -42,7 +45,7 @@ class physical_raven:
         self.i = 0
         self.speed = 10.00 / self.loop_rate
         self.rampup_speed = 0.5 / self.loop_rate
-        self.man_steps = 30
+        self.man_steps = 30 # 30 * (prd.COMMAND_RATE / 1000)
 
         self.homed = [False, False]
         self.moved = [False, False]
@@ -51,7 +54,7 @@ class physical_raven:
 
         # print("\nHoming...\n")
         # self.home_fast()
-        self.set_curr_tm()
+        self.set_curr_tm(True)
         print(self.curr_tm)
         print(self.start_jp)
         # self.resume()
@@ -67,7 +70,7 @@ class physical_raven:
         self.arm_ctl_l.pub_state_command('pause')
         self.arm_ctl_r.pub_state_command('pause')
 
-    def set_curr_tm(self):
+    def set_curr_tm(self, p5=False):
         success = False
         while not success:
             time.sleep(1)
@@ -84,17 +87,19 @@ class physical_raven:
 
         for i in range(len(self.arms)):
             self.next_jp[i] = self.start_jp[i]
-            # self.curr_tm[i] = np.matmul(fk.fwd_kinematics_p5(i, self.start_jp[i], prd), prd.Z_ROT[i])
-            self.curr_tm[i] = fk.fwd_kinematics_p5(i, self.start_jp[i], prd)
-            # self.curr_tm[i] = np.matmul(fk.fwd_kinematics_p5(i, self.start_jp[i], prd), prd.X_ROT)
+            self.next_jp[i] = self.start_jp[i].copy()
+            if p5:
+                self.curr_tm[i] = fk.fwd_kinematics_p5(i, self.start_jp[i], prd)
+            else:
+                self.curr_tm[i] = fk.fwd_kinematics(i, self.start_jp[i], prd)
 
 
     def home_fast(self):
 
-        self.set_curr_tm()
+        self.set_curr_tm(True)
         self.next_jp = [self.home_joints, self.home_joints]
         self.move()
-        self.set_curr_tm()
+        self.set_curr_tm(True)
 
         # for j in range(len(self.moved)):
         #     self.homed[j] = self.moved[j]
@@ -408,7 +413,7 @@ class physical_raven:
         for i in range(increments):
             self.arms[0].pub_jr_command(self.arms[0].seven2sixteen(self.jr[0]))
             self.arms[1].pub_jr_command(self.arms[1].seven2sixteen(self.jr[1]))
-            # time.sleep(prd.COMMAND_TIME)
+            time.sleep(prd.COMMAND_TIME)
 
     # def move_now(self, arm):
     #     """

physical_raven_def.py
@@ -24,24 +24,24 @@ COMMAND_TIME = 1 / COMMAND_RATE
 Eps          = sys.float_info.epsilon
 
 MAX_JR             = np.array([5*Deg2Rad, 5*Deg2Rad, 0.02, 15*Deg2Rad, 15*Deg2Rad, 15*Deg2Rad, 15*Deg2Rad], dtype="float") / COMMAND_RATE
-MAX_JOINTS         = np.array([  m.pi,         m.pi,  0.10,            m.pi,   2, (m.pi * 3)/4, (m.pi * 3)/4],  dtype = 'float')
-MIN_JOINTS         = np.array([ -m.pi,        -m.pi, -0.17,           -m.pi,  -2,            0,            0],  dtype = 'float')
-HOME_JOINTS        = np.array([0.5217216610908508, 1.5799641609191895, 0.40040287375450134, 0.06240818649530411, -0.006050971802324057, 0.7633852362632751, 0.7971110939979553],dtype = 'float')
-HOME_DH            = np.array([[ 0.52172166,  1.57996416,  0.40040287,  0.06240819, 0, 0, 0.79711109],
-                                [ 0.52172166,  1.57996416,  0.40040287,  0.06240819, 0,  0, 0.79711109]],
-                                dtype="float")
-DANCE_SCALE_JOINTS = np.array([   0.3,          0.3,  0.06,             0.3, 1.2,       m.pi/6,       m.pi/6],  dtype = 'float')
+MAX_JOINTS         = np.array([90 * Deg2Rad, 135 * Deg2Rad, 0.56, m.pi, 2, (m.pi * 3)/4, (m.pi * 3)/4],  dtype = 'float')
+MIN_JOINTS         = np.array([0, 45 * Deg2Rad, 0.23, -m.pi, -2, 0, 0],  dtype = 'float')
+HOME_JOINTS        = np.array([m.pi / 6, m.pi / 2, 0.4, 0, 0, m.pi / 4, m.pi / 4], dtype='float')
+# HOME_JOINTS        = np.array([0.5217216610908508, 1.5799641609191895, 0.40040287375450134, 0.06240818649530411, -0.006050971802324057, 0.7633852362632751, 0.7971110939979553],dtype = 'float')
+HOME_DH            = np.array([[0.52359878, 1.57079633, 0.4, 0., 0., 0., 0.78539816],
+                               [0.52359878, 1.57079633, 0.4, 0., 0., -0., 0.78539816]], dtype="float")
+DANCE_SCALE_JOINTS = np.array([0.3, 0.3, 0.06, 0.3, 1.2, m.pi/6, m.pi/6],  dtype='float')
 
-RAVEN_JOINT_LIMITS = np.array([[    0.0,          m.pi/4,           0.23, -m.pi*2, -2, -2, -2],
+RAVEN_JOINT_LIMITS = np.array([[0.0, m.pi/4, 0.23, -m.pi*2, -2, -2, -2],
                                [m.pi/2, (m.pi*3)/4, 0.56, m.pi*2, 2, 2, 2]])
-RAVEN_DH_ALPHA     = np.array([[     0, np.deg2rad(75), np.deg2rad(128),     0, m.pi/2, m.pi/2, 0],
-                               [  m.pi,  np.deg2rad(-75),  np.deg2rad(52),     0, m.pi/2, m.pi/2, 0]], dtype = 'float')
-RAVEN_DH_THETA     = np.array([[     V,               V,          m.pi/2,     V,      V,      V, 0],
-                               [     V,               V,         -m.pi/2,     V,      V,      V, 0]], dtype = 'float')
-RAVEN_DH_A         = np.array([[     0,               0,               0,     0,      0,  0.013, 0],
-                               [     0,               0,               0,     0,      0,  0.013, 0]], dtype = 'float')
-RAVEN_DH_D         = np.array([[     0,               0,               V, -0.482,      0,      0, 0],
-                               [     0,               0,               V, -0.482,      0,      0, 0]], dtype = 'float')
+RAVEN_DH_ALPHA     = np.array([[0, np.deg2rad(75), np.deg2rad(128), 0, m.pi/2, m.pi/2, 0],
+                               [m.pi, np.deg2rad(-75), np.deg2rad(52), 0, m.pi/2, m.pi/2, 0]], dtype = 'float')
+RAVEN_DH_THETA     = np.array([[V, V, m.pi/2, V, V, V, 0],
+                               [V, V, -m.pi/2, V, V, V, 0]], dtype='float')
+RAVEN_DH_A         = np.array([[0, 0, 0, 0, 0, 0.013, 0],
+                               [0, 0, 0, 0, 0, 0.013, 0]], dtype='float')
+RAVEN_DH_D         = np.array([[0, 0, V, -0.47, 0, 0, 0],
+                               [0, 0, V, -0.47, 0, 0, 0]], dtype='float')
 
 RAVEN_IKIN_PARAM   = np.array([float(m.sin(RAVEN_DH_ALPHA[0][1])),
                                 float(m.cos(RAVEN_DH_ALPHA[0][1])),
@@ -53,8 +53,8 @@ RAVEN_T_B0         = np.array([np.matrix([[0, 0,  1,  0.30071], [0, -1, 0, 0.061
                                np.matrix([[0, 0, -1, -0.30071], [0,  1, 0, 0.061], [1, 0, 0, -0.007], [0, 0, 0, 1]], dtype = 'float')])
 RAVEN_T_CB         = np.matrix([[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, 1, 0], [0, 0, 0, 1]], dtype='float') #using quaternion to rotational translator
 
-Z_ROT = np.array([np.matrix([[m.cos(np.deg2rad(25)), -m.sin(np.deg2rad(25)), 0, 0], [m.sin(np.deg2rad(25)), m.cos(np.deg2rad(25)), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype='float'),
-                  np.matrix([[m.cos(np.deg2rad(-25)), -m.sin(np.deg2rad(-25)), 0, 0], [m.sin(np.deg2rad(-25)), m.cos(np.deg2rad(-25)), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype='float')])
+Z_ROT = np.array([np.matrix([[m.cos(np.deg2rad(-25)), -m.sin(np.deg2rad(-25)), 0, 0], [m.sin(np.deg2rad(-25)), m.cos(np.deg2rad(-25)), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype='float'),
+                  np.matrix([[m.cos(np.deg2rad(25)), -m.sin(np.deg2rad(25)), 0, 0], [m.sin(np.deg2rad(25)), m.cos(np.deg2rad(25)), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype='float')])
 
 x_rot_val = 30
 X_ROT = np.matrix([[1, 0, 0, 0],

raven_fk.py
@@ -2,6 +2,7 @@
 import math as m
 import numpy as np
 import ambf_raven_def as ard
+import physical_raven_def as prd
 
 def joint_to_dhvalue(joint, arm, raven_def):
     success = False
@@ -13,6 +14,7 @@ def joint_to_dhvalue(joint, arm, raven_def):
             if i == 5:
                 if arm == 0:
                     dhvalue[i] =  (joint[i] - joint[i+1])
+
                 else:
                     dhvalue[i] = -(joint[i] - joint[i+1])
             else:
@@ -86,13 +88,15 @@ def fwd_kinematics(arm, input_joint_pos, raven_def):
     if raven_def.RAVEN_TYPE:
         # output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_CB, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
         # output_transformation = fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d)
-        output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
-
+        # output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
+        # output_transformation = np.matmul(raven_def.Z_ROT[arm], fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
+        output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_B0[arm], raven_def.Z_ROT[arm]), fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
     else:
         # output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_CB, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
         # output_transformation = np.matmul(raven_def.RAVEN_T_B0[arm], fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
-        output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
-
+        # output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
+        # output_transformation = np.matmul(raven_def.RAVEN_T_B0[arm], fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
+        output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_B0[arm], raven_def.Z_ROT[arm]), fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
     return output_transformation
 
 
@@ -127,20 +131,33 @@ def fwd_kinematics_p5(arm, input_joint_pos, raven_def):
     if raven_def.RAVEN_TYPE:
         # output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_CB, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
         # output_transformation = np.matmul(raven_def.RAVEN_T_B0[arm], fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
-        output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
+        # output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
+        # output_transformation = np.matmul(raven_def.Z_ROT[arm], fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
+        output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_B0[arm], raven_def.Z_ROT[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
+
 
     else:
         # output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_CB, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
         # output_transformation = np.matmul(raven_def.RAVEN_T_B0[arm], fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
-        output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
-
+        # output_transformation = np.matmul(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
+        # output_transformation = fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d)
+        # output_transformation = np.matmul(raven_def.RAVEN_T_B0[arm], fwd_trans(0, 6, dh_alpha, dh_theta, dh_a, dh_d))
+        output_transformation = np.matmul(np.matmul(raven_def.RAVEN_T_B0[arm], raven_def.Z_ROT[arm]), fwd_trans(0, 5, dh_alpha, dh_theta, dh_a, dh_d))
 
     return output_transformation
 
-# def main():
-#
-#
-#     dh_vals_l = joint_to_dhvalue(ard.HOME_JOINTS, 0, ard)
-#     print(dh_vals_l)
-#
-# main()
+def main():
+
+
+    # dh_vals_l = joint_to_dhvalue(prd.HOME_JOINTS, 1, ard)
+    # print(dh_vals_l)
+    # print(fwd_kinematics(0, prd.HOME_JOINTS, prd))
+    # print(fwd_kinematics(1, prd.HOME_JOINTS, prd))
+    print(fwd_kinematics_p5(0, prd.HOME_JOINTS, prd))
+    print(fwd_kinematics_p5(1, prd.HOME_JOINTS, prd))
+    # print(fwd_kinematics(0, ard.HOME_JOINTS, ard))
+    # print(fwd_kinematics(1, ard.HOME_JOINTS, ard))
+    print(fwd_kinematics_p5(0, ard.HOME_JOINTS, ard))
+    print(fwd_kinematics_p5(1, ard.HOME_JOINTS, ard))
+
+main()

raven_ik.py
@@ -136,20 +136,20 @@ def inv_kinematics(arm, input_cp, input_gangle, raven_def):
         T_3_6 = np.matmul(np.linalg.inv(T_0_3), T_0_6)
         T_3_6_B = u.get_Basis(T_3_6)
         T_3_6_O = u.get_Origin(T_3_6)
-        c5 = -float(T_3_6_B[2,2])
+        c5 = -float(T_3_6_B[2, 2])
         s5 = float(T_3_6_O[2] - raven_def.RAVEN_IKIN_PARAM[4]) / float(raven_def.RAVEN_IKIN_PARAM[5])
 
         if m.fabs(c5) > raven_def.Eps:
             c4 = float(T_3_6_O[0]) / float(raven_def.RAVEN_IKIN_PARAM[5] * c5)
             s4 = float(T_3_6_O[1]) / float(raven_def.RAVEN_IKIN_PARAM[5] * c5)
         else:
-            c4 = T_3_6_B[0][2] / s5
-            s4 = T_3_6_B[1][2] / s5
+            c4 = T_3_6_B[0, 2] / s5
+            s4 = T_3_6_B[1, 2] / s5
         iksol[i][3] = m.atan2(s4, c4)
         iksol[i][4] = m.atan2(s5, c5)
         if m.fabs(s5) > raven_def.Eps:
-            c6 = T_3_6_B[2,0] / s5
-            s6 = -T_3_6_B[2,1] / s5
+            c6 = T_3_6_B[2, 0] / s5
+            s6 = -T_3_6_B[2, 1] / s5
         else:
             dh_theta[3] = iksol[i][3]
             dh_theta[4] = iksol[i][4]
@@ -190,11 +190,16 @@ def inv_kinematics_p5(arm, input_cp, input_gangle, home_dh, raven_def):
     # p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.RAVEN_T_CB, raven_def.RAVEN_T_B0[arm])), input_cp)
     # p05_dh = np.matmul(np.linalg.inv(raven_def.RAVEN_T_B0[arm]), input_cp)
     if raven_def.RAVEN_TYPE:
-        p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm])), input_cp)
+        # p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm])), input_cp)
+        p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.RAVEN_T_B0[arm], raven_def.Z_ROT[arm])), input_cp)
+
     else:
         # p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.RAVEN_T_CB, raven_def.RAVEN_T_B0[arm])), input_cp)
+        # p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm])), input_cp)
+        # p05_dh = input_cp
         # p05_dh = np.matmul(np.linalg.inv(raven_def.RAVEN_T_B0[arm]), input_cp)
-        p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.X_ROT, raven_def.RAVEN_T_B0[arm])), input_cp)
+        p05_dh = np.matmul(np.linalg.inv(np.matmul(raven_def.RAVEN_T_B0[arm], raven_def.Z_ROT[arm])), input_cp)
+
 
     p05 = np.array([p05_dh[0, 3], p05_dh[1, 3], p05_dh[2, 3], 1.0], dtype="float")
 